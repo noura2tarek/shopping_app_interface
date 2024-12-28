53a0dc0c7d0f34fe4034c85fb29023f464a15da0
Noura Tarek <99992263+noura2tarek@users.noreply.github.com>
2024-12-28 23:33:31 +0200

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # shopping_app_interface
+
+Flutter Task3:
+
+Shopping app displays products and offers in one page using flutter.
+
+products data shown using page view and grid view,
+And offers data shown using list view.
+
+The UI is made responsive using media queries,  
+The widgets in lib/widgets folder.    
+The screens in lib/screens.
+The font used is Suwannaphum.    
+Other App components is stored in lib/utils folder.
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_app_interface/screens/home_page.dart';
+import 'package:shopping_app_interface/utils/app_strings.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,18 +8,18 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: AppStrings.shoppingApp,
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: 'Suwannaphum',
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  HomePage(),
+      home: HomePage(),
     );
   }
 }
