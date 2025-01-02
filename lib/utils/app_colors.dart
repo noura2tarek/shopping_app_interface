aabbc1d6cb9c047ad4ba4385a9a5b44b34b4743f
Noura Tarek <99992263+noura2tarek@users.noreply.github.com>
2025-01-02 23:51:42 +0200

    Update interface and add shadow

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -4,4 +4,6 @@ class AppColors {
   static const Color mainColor = Colors.orange;
   static const Color black87 = Colors.black87;
   static const Color white = Colors.white;
+  static const Color grey = Colors.grey;
+  static Color? grey300 = Colors.grey[300];
 }
