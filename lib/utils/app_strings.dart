c2de09b8cfb1336c9723ff4aff316dc950562ab8
Noura Tarek <99992263+noura2tarek@users.noreply.github.com>
2025-01-02 23:29:31 +0200

    Update animation

--- lib/utils/app_strings.dart
+++ lib/utils/app_strings.dart
@@ -7,14 +7,14 @@ class AppStrings {
   static const String enterYourFullName = 'Enter your full name..';
   static const String pleaseEnterYourName = 'Please enter your name';
   static const String fullNameMustStart =
-      'The full name must start with a capital letter';
+      'The full name must start with a capital letter.';
   static const String enterYourEmail = 'Enter your email..';
   static const String pleaseEnterYourEmail = 'Please enter your email';
   static const String pleaseEnterValidEmail = 'Please enter valid email';
   static const String enterYourPassword = 'Enter your password..';
   static const String passwordsCannotBeEmpty = 'password can\'t be empty';
   static const String passwordsMustBeMore =
-      'password must be more than 6 characters';
+      'password must be more than 6 characters.';
   static const String enterYourConfirmPassword = 'Enter your confirm password..';
   static const String passwordsNotMatch = 'passwords don\'t match, try again';
   static const String accountCreatedSuccessfully = 'Account created successfully!';
