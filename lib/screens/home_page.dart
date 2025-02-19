5040f1880426c7c7cd3d649589400dfdb5ade36b
Noura Tarek <99992263+noura2tarek@users.noreply.github.com>
2025-02-19 18:29:31 +0200

    Edit grid view item

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -78,7 +78,9 @@ class _HomePageState extends State<HomePage> {
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 7.0),
               child: ProductsGridView(
-                  onToggleItemInCartFunction: _onToggleItemInCart),
+                onAddItemInCartFunction: _onAddItemInCart,
+                cartItems: _cartItems,
+              ),
             ),
             /* --- Hot offers text ---*/
             CustomText(text: AppStrings.hotOffers),
@@ -98,24 +100,14 @@ class _HomePageState extends State<HomePage> {
 
   ////////////////////////////
   //----------- Methods--------------//
-  // toggle item in cart function
-  void _onToggleItemInCart(String itemName) {
-    var isExist = _cartItems.contains(itemName);
-    if (isExist) {
-      setState(() {
-        _cartItems.remove(itemName);
-      });
-      ScaffoldMessenger.of(context).showSnackBar(
-        customSnackBar(text: AppStrings.itemRemoved),
-      );
-    } else {
-      setState(() {
-        _cartItems.add(itemName);
-      });
-      ScaffoldMessenger.of(context).showSnackBar(
-        customSnackBar(text: AppStrings.itemAdded),
-      );
-    }
+  // add item in cart function
+  void _onAddItemInCart(String itemName) {
+    setState(() {
+      _cartItems.add(itemName);
+    });
+    ScaffoldMessenger.of(context).showSnackBar(
+      customSnackBar(text: AppStrings.itemAdded),
+    );
   }
 }
 
